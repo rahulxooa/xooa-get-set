7ce4bc6a93698f3e4ae7334383a84b397e13635d
Rahul Kamboj <44227025+rahulxooa@users.noreply.github.com>
2018-11-06 15:44:22 +0530

    Update index.js

--- counter/index.js
+++ counter/index.js
@@ -99,13 +99,53 @@ let Chaincode = class {
     let endKey = args[1];
 
     let iterator = await stub.getStateByRange(startKey,endKey); //get the car from chaincode state
-    console.log(iterator)
-    
-    if (!carAsBytes || carAsBytes.toString().length <= 0) {
-      throw new Error(carNumber + " does not exist: ");
+
+    while(true){
+     let method = thisClass['getAllResults'];
+    let results = await method(iterator, false);
+
+    return Buffer.from(JSON.stringify(results));
     }
-    console.log(carAsBytes.toString());
-    return carAsBytes;
+    
+   
+  }
+async getAllResults(iterator, isHistory) {
+    let allResults = [];
+    while (true) {
+      let res = await iterator.next();
+
+      if (res.value && res.value.value.toString()) {
+        let jsonRes = {};
+        console.log(res.value.value.toString('utf8'));
+
+        if (isHistory && isHistory === true) {
+          jsonRes.TxId = res.value.tx_id;
+          jsonRes.Timestamp = res.value.timestamp;
+          jsonRes.IsDelete = res.value.is_delete.toString();
+          try {
+            jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Value = res.value.value.toString('utf8');
+          }
+        } else {
+          jsonRes.Key = res.value.key;
+          try {
+            jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+          } catch (err) {
+            console.log(err);
+            jsonRes.Record = res.value.value.toString('utf8');
+          }
+        }
+        allResults.push(jsonRes);
+      }
+      if (res.done) {
+        console.log('end of data');
+        await iterator.close();
+        console.info(allResults);
+        return allResults;
+      }
+    }
   }
 
   async decrement(stub, args) {
